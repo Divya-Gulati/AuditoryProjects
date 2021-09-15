00f2ea5679404b74e9a1388e6c3825b23bc79a59
Divya-Gulati <60729240+Divya-Gulati@users.noreply.github.com>
2021-09-15 16:13:54 +0530

    Saves average referencing mat file

---
+++ auditoryGammaProjectCodes/SaveReferencing.m
@@ -0,0 +1,36 @@
+%%%%%% creating and saving vals for average referencing %%%%%
+clear;clc;
+[subjectName,expDate,protocolNames,good_elecs] = EcogAuditoryGammaData;
+folderSourceString = 'D:\OneDrive - Indian Institute of Science\divya\NimhansRippleProject\Divya_AuditoryProjects\data\humanECoG';
+gridType = 'ECoG';
+
+%%
+for id = 4%1:length(subjectName)
+    
+    % Get folders
+    folderName = string(fullfile(folderSourceString,'data',subjectName{id},gridType,expDate{id},protocolNames{id}));
+    folderExtract = fullfile(folderName,'extractedData');
+    folderSegment = fullfile(folderName,'segmentedData');
+    folderLFP = fullfile(folderSegment,'LFP');
+    
+    
+    x = load(fullfile(folderLFP,'lfpInfo.mat'));
+    AllElectrode=sort(x.analogChannelsStored);
+    ElecSet = intersect(AllElectrode,cell2mat(good_elecs{id}));
+    
+    
+    all_elec_Data = [];
+    for i = 1:length(ElecSet)%elecs
+        ElectrodeData = load(fullfile(folderLFP, ['elec' num2str(ElecSet(i)) '.mat']));
+        all_elec_Data = [all_elec_Data;ElectrodeData.analogData];
+    end
+    
+    analogData= mean(all_elec_Data);
+    analogData= repmat(analogData,size(ElectrodeData.analogData,1),1);
+    save(fullfile(folderLFP,'\AvgRef.mat'),'analogData');
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
+
+
+
