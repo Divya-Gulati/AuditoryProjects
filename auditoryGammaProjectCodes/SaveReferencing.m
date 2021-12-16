2d59cbf6e04aa5a94d841a0382e6dec14620d9af
Divya-Gulati <60729240+Divya-Gulati@users.noreply.github.com>
2021-12-16 17:26:53 +0530

    update Savereferencing.m

--- auditoryGammaProjectCodes/SaveReferencing.m
+++ auditoryGammaProjectCodes/SaveReferencing.m
@@ -5,30 +5,31 @@ folderSourceString = 'D:\OneDrive - Indian Institute of Science\divya\NimhansRip
 gridType = 'ECoG';
 
 %%
-for id = 4%1:length(subjectName)
-    
-    % Get folders
-    folderName = string(fullfile(folderSourceString,'data',subjectName{id},gridType,expDate{id},protocolNames{id}));
-    folderExtract = fullfile(folderName,'extractedData');
-    folderSegment = fullfile(folderName,'segmentedData');
-    folderLFP = fullfile(folderSegment,'LFP');
-    
-    
-    x = load(fullfile(folderLFP,'lfpInfo.mat'));
-    AllElectrode=sort(x.analogChannelsStored);
-    ElecSet = intersect(AllElectrode,cell2mat(good_elecs{id}));
-    
-    
-    all_elec_Data = [];
-    for i = 1:length(ElecSet)%elecs
-        ElectrodeData = load(fullfile(folderLFP, ['elec' num2str(ElecSet(i)) '.mat']));
-        all_elec_Data = [all_elec_Data;ElectrodeData.analogData];
+for id = 6%1:length(subjectName)
+    for iid = 1:length(protocolNames{id})
+        % Get folders
+        protocolName = cell2mat(protocolNames{id}(:,iid)) ;
+        folderName = string(fullfile(folderSourceString,'data',subjectName{id},gridType,expDate{id},protocolName));
+        folderExtract = fullfile(folderName,'extractedData');
+        folderSegment = fullfile(folderName,'segmentedData');
+        folderLFP = fullfile(folderSegment,'LFP');
+        
+        
+        x = load(fullfile(folderLFP,'lfpInfo.mat'));
+        AllElectrode=sort(x.analogChannelsStored);
+        ElecSet = intersect(AllElectrode,cell2mat(good_elecs{id}));
+        
+        
+        all_elec_Data = [];
+        for i = 1:length(ElecSet)%elecs
+            ElectrodeData = load(fullfile(folderLFP, ['elec' num2str(ElecSet(i)) '.mat']));
+            all_elec_Data = [all_elec_Data;ElectrodeData.analogData];
+        end
+        
+        analogData= mean(all_elec_Data);
+        analogData= repmat(analogData,size(ElectrodeData.analogData,1),1);
+        save(fullfile(folderLFP,'\AvgRef.mat'),'analogData');
     end
-    
-    analogData= mean(all_elec_Data);
-    analogData= repmat(analogData,size(ElectrodeData.analogData,1),1);
-    save(fullfile(folderLFP,'\AvgRef.mat'),'analogData');
-    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
 
