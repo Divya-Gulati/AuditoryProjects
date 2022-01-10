519b6e4e8758e0bb05a3af8895e7fe125fc586bd
Divya-Gulati <60729240+Divya-Gulati@users.noreply.github.com>
2022-01-10 13:09:13 +0530

    Added condition file and timing file for running longer duration sound files for ASSR protocol

---
+++ commonCodes/Protocols/GeneratingConditionsFile_Aim2_Exp1_Dur1500.m
@@ -0,0 +1,26 @@
+%%%%%%%%%% generating conditions file for for experiment 1  %%%%%%%%%%%%
+
+%%%% opening conditions file %%%%
+folderSourceString = 'D:\OneDrive - Indian Institute of Science\divya\NimhansRippleProject\Divya_AuditoryProjects\programs';
+folderName = fullfile(folderSourceString,'AuditoryProjects','commonCodes','Protocols');
+fid =fopen(fullfile(folderName,'ASSR_exp1_longDur.txt'),'w');
+fprintf(fid,'%s\t%s\t%s\t%s\t%s\t','Condition','Frequency','Block','Timing File','TaskObject#1');
+
+% for generating sound file name
+
+modIndex = [0.031250,0.0625,0.125,0.25,0.5,1];
+modFreq = 20:4:60;
+
+
+count = 1;
+for mf = modFreq
+    for mi = modIndex
+        str = sprintf('snd(Azi_0.0_Elev_0.0_Type_2_RF_1000.0_RP_0.0_MD_%2.1f _RV_%1.1f _Dur_1500.wav)',mi*100,mf);
+        fprintf(fid,'\n%d\t%d\t%d\t%s\t%s',count,1,1,'timingfile_assr_exp1_longDur', str);
+        count = count+1;
+    end
+end
+ 
+fclose(fid);
+
+
---
+++ commonCodes/Protocols/timingfile_assr_exp1_longDur.m
@@ -0,0 +1,3 @@
+toggleobject(1,'eventmarker',7);
+idle(1600);
+trialerror(0);
